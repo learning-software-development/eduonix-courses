c0f2ea17888b45d3b069d06034c6dc3fefbea16a
Quintin Henn <quintin.henn@gmail.com>
2021-08-26 15:36:02 +0200

    Learning BackboneJS programming

--- learn-nodejs-programming-from-scratch/backbonejs-app/src/scripts/main.js
+++ learn-nodejs-programming-from-scratch/backbonejs-app/src/scripts/main.js
@@ -2,12 +2,12 @@ import Backbone from 'backbone';
 import _ from 'underscore';
 // import $ from 'jquery';
 
-let UserView = Backbone.View.extend({
+const UserView = Backbone.View.extend({
   el: '#user-view',
   template: _.template(Backbone.$('#user-tmpl').html()),
 
   initialize: function () {
-    console.log("UserView created!");
+    console.log("UserView was initialize!");
     this.render();
   },
 
@@ -26,3 +26,42 @@ let UserView = Backbone.View.extend({
 });
 
 let userView = new UserView();
+
+const UserModel = Backbone.Model.extend({
+  initialize: function () {
+    console.log("UserModel was initialize!");
+
+    this.on("change:email", function () {
+      console.warn("The user email was changed!");
+    })
+  },
+  defaults: {
+    username: "JohnDoe",
+    email: "johndoe@gmail.com"
+  },
+  getUserEmail: function () {
+    return this.get('email');
+  },
+  setUserEmail: function (newEmail) {
+    this.set({ email: newEmail });
+  }
+});
+
+let user = new UserModel();
+console.dir(user);
+
+console.log(user.getUserEmail());
+
+let brad = new UserModel({
+  username: "bradT",
+  email: "bradt@gmail.com"
+});
+
+console.log(brad.getUserEmail());
+brad.setUserEmail("bread66@yahoo.com");
+console.log(brad.getUserEmail());
+
+const Users = Backbone.Collection.extend({
+  model: UserModel,
+  url: 'data.json'
+});
